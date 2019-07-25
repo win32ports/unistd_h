df714c366b1924c8ed4a66496a79fe45c3b33ccc
SSE4 <tomskside@gmail.com>
2019-07-25 10:23:03 -0700

    - add S_ISREG and friends
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- example.c
+++ example.c
@@ -4,17 +4,43 @@
 
 #include <unistd.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main()
 {
+	struct stat s;
+
 	assert(access("dummy", R_OK) == -1);
 	assert(access("dummy", W_OK) == -1);
 	assert(access("dummy", X_OK) == -1);
 	assert(access("dummy", F_OK) == -1);
 
+	FILE* f = fopen("dummy", "wb");
+	assert(f);
+	fclose(f);
+
+	stat("dummy", &s);
+
+	assert(S_ISREG(s.st_mode));
+	assert(!S_ISDIR(s.st_mode));
+
+	assert(access("dummy", R_OK) == 0);
+	assert(access("dummy", W_OK) == 0);
+	assert(access("dummy", X_OK) == 0);
+	assert(access("dummy", F_OK) == 0);
+
+	unlink("dummy");
+
 	int fd2 = dup(STDIN_FILENO);
 	assert(fd2 != STDIN_FILENO);
 	close(fd2);
-	_mkdir("temp");
+
+	assert(rmdir("temp") == -1);
+	assert(_mkdir("temp") == 0);
+
+	stat("temp", &s);
+	assert(!S_ISREG(s.st_mode));
+	assert(S_ISDIR(s.st_mode));
+
 	assert(rmdir("temp") == 0);
 }
--- unistd.h
+++ unistd.h
@@ -45,6 +45,8 @@ extern "C" {
 #include <process.h> /* _execl() */
 #endif /* _INC_PROCESS */
 
+#include <sys/stat.h> /* */
+
 #ifndef access
 #define access _access
 #endif /* access */
@@ -120,6 +122,10 @@ extern "C" {
 #ifndef rmdir
 #define rmdir _rmdir
 #endif /* rmdir */
+
+#ifndef unlink
+#define unlink _unlink
+#endif /* unlink */
 
 /* permission bits below must be defined in sys/stat.h, but MSVC lacks them */
 
@@ -202,6 +208,102 @@ extern "C" {
 #ifndef S_IXUGO
 #define S_IXUGO 0111
 #endif /* S_IXUGO */
+
+#ifndef _S_IFMT
+#define _S_IFMT 0xF000
+#endif /* _S_IFMT */
+
+#ifndef _S_IFIFO
+#define _S_IFIFO 0x1000
+#endif /* _S_IFIFO */
+
+#ifndef _S_IFCHR
+#define _S_IFCHR 0x2000
+#endif /* _S_IFCHR */
+
+#ifndef _S_IFDIR
+#define _S_IFDIR 0x4000
+#endif /* _S_IFDIR */
+
+#ifndef _S_IFBLK
+#define _S_IFBLK 0x6000
+#endif /* _S_IFBLK */
+
+#ifndef _S_IFREG
+#define _S_IFREG 0x8000
+#endif /* _S_IFREG */
+
+#ifndef _S_IFLNK
+#define _S_IFLNK 0xA000
+#endif /* _S_IFLNK */
+
+#ifndef _S_IFSOCK
+#define _S_IFSOCK 0xC000
+#endif /* _S_IFSOCK */
+
+#ifndef S_IFMT
+#define S_IFMT _S_IFMT
+#endif /* S_IFMT */
+
+#ifndef S_IFIFO
+#define S_IFIFO _S_IFIFO
+#endif /* S_IFIFO */
+
+#ifndef S_IFCHR
+#define S_IFCHR _S_IFCHR
+#endif /* S_IFCHR */
+
+#ifndef S_IFDIR
+#define S_IFDIR _S_IFDIR
+#endif /* S_IFDIR */
+
+#ifndef S_IFBLK
+#define S_IFBLK _S_IFBLK
+#endif /* S_IFBLK */
+
+#ifndef S_IFREG
+#define S_IFREG _S_IFREG
+#endif /* S_IFREG */
+
+#ifndef S_IFLNK
+#define S_IFLNK _S_IFLNK
+#endif /* S_IFLNK */
+
+#ifndef S_IFSOCK
+#define S_IFSOCK _S_IFSOCK
+#endif /* S_IFSOCK */
+
+#ifndef S_ISTYPE
+#define S_ISTYPE(mode, mask) (((mode) & S_IFMT) == (mask))
+#endif /* S_ISTYPE */
+
+#ifndef S_ISFIFO
+#define S_ISFIFO(mode) S_ISTYPE(mode, S_IFIFO)
+#endif /* S_ISFIFO */
+
+#ifndef S_ISCHR
+#define S_ISCHR(mode) S_ISTYPE(mode, S_IFCHR)
+#endif /* S_ISCHR */
+
+#ifndef S_ISDIR
+#define S_ISDIR(mode) S_ISTYPE(mode, S_IFDIR)
+#endif /* S_ISDIR */
+
+#ifndef S_ISBLK
+#define S_ISBLK(mode) S_ISTYPE(mode, S_IFBLK)
+#endif /* S_ISBLK */
+
+#ifndef S_ISREG
+#define S_ISREG(mode) S_ISTYPE(mode, S_IFREG)
+#endif /* S_ISREG */
+
+#ifndef S_ISLNK
+#define S_ISLNK(mode) S_ISTYPE(mode, S_IFLNK)
+#endif /* S_ISLNK */
+
+#ifndef S_ISSOCK
+#define S_ISSOCK(mode) S_ISTYPE(mode, S_IFSOCK)
+#endif /* S_ISSOCK */
 
 #ifdef __cplusplus
 }
